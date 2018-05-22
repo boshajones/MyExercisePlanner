145d10246f9dc6fbb8e0629358f7f662a79609bf
Daniel <daniel.sims1994@outlook.com>
2018-05-22 18:10:21 +0100

    Initial commit

---
+++ app/src/main/java/com/myexerciseplanner/myexerciseplanner/MainActivity.java
@@ -0,0 +1,13 @@
+package com.myexerciseplanner.myexerciseplanner;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
